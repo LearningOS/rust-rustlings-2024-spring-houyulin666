c1dfb06729bff0deecd94f2ca87e00d9b5f81063
houyulin666 <1793811309@qq.com>
2024-04-10 01:51:42 +0800

    Day2提交

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -9,24 +9,48 @@
 
 // I AM NOT DONE
 
+
+
+// fn main() {
+//     let vec0 = Vec::new();
+
+//     let mut vec1 = fill_vec(&vec0);
+
+//     println!("{} has length {}, with contents: `{:?}`", "vec0", vec0.len(), vec0);
+
+//     vec1.push(88);
+
+//     println!("{} has length {}, with contents `{:?}`", "vec1", vec1.len(), vec1);
+// }
+
+// fn fill_vec(vec: &Vec<i32>) -> Vec<i32> {
+//     let mut vec = vec.clone();
+//     vec.push(22);
+//     vec.push(44);
+//     vec.push(66);
+
+//     vec
+// }
+
+
 fn main() {
-    let vec0 = Vec::new();
+    let mut vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
+    fill_vec(&mut vec0);
 
-    println!("{} has length {}, with contents: `{:?}`", "vec0", vec0.len(), vec0);
-
+    let vec1 = &mut vec0;
+    // println!("{} has length {}, with contents: `{:?}`", "vec0", vec0.len(), vec0);
+    fill_vec(vec1);
     vec1.push(88);
+
+    //要是注掉上面的一行，执行下面的一行，会报错。说明同一时间，只有一个可变存储空间，无论是否借用。还有就是借用不会被销毁
+    //vec0.push(88);
 
     println!("{} has length {}, with contents `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
-
+fn fill_vec(vec: &mut Vec<i32>){
     vec.push(22);
     vec.push(44);
     vec.push(66);
-
-    vec
 }
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,11 +13,17 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
 // fn calculate_price_of_apples {
-
+fn calculate_price_of_apples(nums:u32)->u32{
+    if(nums<=40){
+        return nums*2;
+    }
+    else {
+        return nums;
+    }
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
